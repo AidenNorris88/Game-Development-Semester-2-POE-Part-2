52cec26ba5ee14c17c285b40bda62c2e1d4ec258
Aiden Norris <ST10483324@vgconnect.edu.za>
2025-08-31 21:42:27 +0200

    added empty tile thing

--- GADE POE Part 1/GADE POE Part 1/EmptyTile.cs
+++ GADE POE Part 1/GADE POE Part 1/EmptyTile.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GADE_POE_Part_1
 {
-    internal class EmptyTile
+    internal class EmptyTile: Tile
     {
-
+        public  EmptyTile(Position position): base ( position)
+        {
+           
+        }
+        public override char Display
+        {
+            get { return '.'; }
+        }
+           
     }
 }
